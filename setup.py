7ef2ea8ecfdaea975fea9384d82ca239d164a2d3
David Schenck <david.schenck@outlook.com>
2021-05-02 17:57:07 +0100

    versioning

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="easytree",
-    version="0.1.6",
+    version="0.1.7",
     author="david.schenck@outlook.com",
     author_email="david.schenck@outlook.com",
     description="A fluent tree builder, useful to create multi-level, nested JSON configurations.",
